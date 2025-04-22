fa5cb41ae5b0fdc15ae52767de50b625d305f55a
Joel Burleson <joelburleson0104@gmail.com>
2025-04-22 17:32:29 +0200

    add: added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="learn2slither",
+    description="A AI agent trained to play the game snake. Train your own snake or test a pre-trained one.",
+    long_description=open("README.md").read(),
+    packages=find_packages(),
+    version="1.0.0",
+    install_requires=["numpy", "torch"],
+    author="Joel Burleson",
+    url="https://github.com/fburleson/learn2slither",
+    python_requires=">=3.10",
+)
